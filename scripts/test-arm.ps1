5fba69315746e3fe57c826587e2a30d0d186a909
Mike <mbenko@benko.com>
2024-07-31 16:48:58 -0500

    Add script to deploy using az webapp up

--- scripts/test-arm.ps1
+++ scripts/test-arm.ps1
@@ -14,3 +14,9 @@ $rgName = "rg-$appName-$envName"
 ## Create RG
 az group create --name $rgName --location $loc --tag "env=$envName" "app=$appName" "createDate=$(Get-Date -Format 'yyyy-MM-dd')"
 az group show 
+
+$planName = "$appName-$envName-plan"
+$siteName = "$appName-$envName-site"
+
+cd src/myapp
+az webapp up -g $rgName --plan $planName --name $siteName  --launch-browser
