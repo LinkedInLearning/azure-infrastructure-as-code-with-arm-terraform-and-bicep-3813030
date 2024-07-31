f40af6cefa726d5127815cd90fe9a7f920d0fee2
Mike <mbenko@benko.com>
2024-07-31 13:59:00 -0500

    Add test-azcli commands

---
+++ scripts/test-azcli.ps1
@@ -0,0 +1,11 @@
+az
+az login --help
+az login --use-device-code
+az account show
+az account show --help
+az account show --output table
+az account show -o table
+az group list -o table
+$rg = "rg-learn-iac-arm"
+az group show -n $rg
+
