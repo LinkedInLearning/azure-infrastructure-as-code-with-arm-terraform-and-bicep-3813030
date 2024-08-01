4d46321173b05891f7b98c1dbc3e6578a00cd2fb
Mike <mbenko@benko.com>
2024-08-01 10:32:43 -0500

    add parameters

--- scripts/test-arm.ps1
+++ scripts/test-arm.ps1
@@ -15,6 +15,14 @@ $rgName = "rg-$appName-$envName"
 az group create --name $rgName --location $loc --tag "env=$envName" "app=$appName" "createDate=$(Get-Date -Format 'yyyy-MM-dd')"
 az group show 
 
+## Deploy IaC
+az deployment group create --what-if -g $rgName --template-file ./infra/arm/mySite.json
+
+az deployment group create -g $rgName --template-file ./infra/arm/mySite.json `
+  --parameters ./infra/arm/mySite.parameters.json
+
+
+## Deploy Code
 $planName = "$appName-$envName-plan"
 $siteName = "$appName-$envName-site"
 
