08f73a8c3f9ec054f2e59faa8809ae46c2295e0d
Mike <mbenko@benko.com>
2024-08-01 15:31:53 -0500

    Script

--- scripts/test-stacks.ps1
+++ scripts/test-stacks.ps1
@@ -1,12 +1,14 @@
 # test-stacks.ps1
 
+az stack sub --help
+
 ## Set Variables
 $appName = "learn-azIaC"
 $loc = "westus"
 
 $stack  = "$appName-stack"
 
-  az stack sub create `
+az stack sub create `
   --name $stack `
   --location $loc `
   --template-file ./infra/arm/main.bicep `
