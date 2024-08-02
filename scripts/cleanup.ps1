1420cc9e3a00edbda01584a5a2722f3b3b4142e4
Mike <mbenko@benko.com>
2024-08-02 12:09:25 -0500

    Add cleanup script

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -1,3 +1,5 @@
+az group list -o table
+
 az group delete --no-wait -y -n rg-learn-azure-IaC
 az group delete --no-wait -y -n rg-azIaC-test
 az group delete --no-wait -y -n rg-azIaC-arm
@@ -9,3 +11,4 @@ az group delete --no-wait -y -n azIaC-pulumi-rgbb43f3dc
 az group delete --no-wait -y -n rg-azIaC-ansible
 az group delete --no-wait -y -n myApp20240730164933ResourceGroup
 az group delete --no-wait -y -n rg-azIaC-bash
+
